4ca138360bab05409c70e9e1fdd8309dc4244997
bigsaddev <chris.doehmann@googlemail.com>
2025-11-17 18:59:42 +0100

    added plot 3

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def main():
     svg.ticks(spacing=1, font_size=8)
 
     functions = [
-        (lambda x: x*2-6, "green"),
-        (lambda x: -0.5*x+2, "blue"),
-        (lambda x: 3*x, "pink"),
-        (lambda x: -6*x-4, "black"),
+        (lambda x: x*2+2, "green"),
+        (lambda x: -2*x+5, "blue"),
+        (lambda x: 2.5*x, "pink"),
+        (lambda x: -x+4, "black"),
     ]
 
     for func, color in functions:
@@ -20,11 +20,9 @@ def main():
     svg.text(-6.5, 4, "y = mx+b", color="black", font_size=18)
     svg.fraction("m = ", "y - b", "x", -4.5, 2, color="black", font_size=18)
 
-    svg.map_coord(1, 3, text="A", color="black", font_size=12)
-    #svg.map_coord(-4, 7, text="B", color="black", font_size=12)
+    
 
-
-    svg.save("plot.svg")
+    svg.save("plot3.svg")
 
 
 if __name__ == "__main__":
